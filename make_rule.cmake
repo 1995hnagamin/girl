5c61af2b219d277506769b60c98de7425f13bfad
Hideaki Nagamine <1995.hnagamin@gmail.com>
2016-02-08 23:22:13 +0900

    Remove CMakeLists.txt in subdirectories

--- make_rule.cmake
+++ make_rule.cmake
@@ -1,7 +1,15 @@
 add_executable(girl src/girl.cpp)
 add_executable(girl-ls src/girl-ls.cpp)
-
+add_library(location lib/location.cpp)
+add_library(utils lib/utils.cpp)
 install (TARGETS girl DESTINATION bin)
+
+target_link_libraries(utils
+  location
+  ${Boost_SYSTEM_LIBRARIES}
+  ${Boost_FILESYSTEM_LIBRARIES}
+  ${Boost_PROGRAM_OPTIONS_LIBRARIES}
+)
 
 target_link_libraries(girl
   location
@@ -16,3 +24,5 @@ target_link_libraries(girl-ls
   utils
   ${Boost_LIBRARIES}
 )
+
+link_directories(${CMAKE_CURRENT_SOURCE_DIR})
