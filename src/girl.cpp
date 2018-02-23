9a351f29e2149b99edca2368c7dfdeb34f5d69c6
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2018-02-24 05:11:37 +0900

    clang-format

--- src/girl.cpp
+++ src/girl.cpp
@@ -1,26 +1,30 @@
-#include <fstream>
-#include <iostream>
-#include <string>
-#include <cstdlib>
-#include <functional>
-#include <numeric>
-#include <algorithm>
-#include <boost/optional.hpp>
-#include <boost/filesystem.hpp>
-#include <boost/algorithm/string.hpp>
-#include <boost/program_options.hpp>
-#include <wordexp.h>
 #include "location.hpp"
+#include <algorithm>
+#include <boost/algorithm/string.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/optional.hpp>
+#include <boost/program_options.hpp>
+#include <cstdlib>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <wordexp.h>
 
-bool does_contain_alias(const std::string &line, const std::string &alias) {
+bool
+does_contain_alias(const std::string &line, const std::string &alias) {
   using namespace std::placeholders;
   if (line.empty() || line[0] != '=') {
     return false;
   }
   std::string aliasline(line.substr(1));
   std::vector<std::string> aliases;
-  boost::algorithm::split(aliases, aliasline, boost::algorithm::is_any_of(","));
-  //return std::any_of(aliases.begin(), aliases.end(), std::bind(std::equal_to, _1, alias));
+  boost::algorithm::split(
+      aliases, aliasline, boost::algorithm::is_any_of(","));
+  // return std::any_of(aliases.begin(), aliases.end(),
+  // std::bind(std::equal_to,
+  // _1, alias));
   for (const std::string &str : aliases) { // XXX
     if (str == alias) {
       return true;
@@ -29,11 +33,14 @@ bool does_contain_alias(const std::string &line, const std::string &alias) {
   return false;
 }
 
-bool is_section_title(const std::string &line) {
+bool
+is_section_title(const std::string &line) {
   return line.length() > 1 && line[0] == '#' && line[1] != '#';
 }
 
-boost::optional<Location> find_section(const boost::filesystem::path &filepath, const std::string &target) {
+boost::optional<Location>
+find_section(const boost::filesystem::path &filepath,
+             const std::string &target) {
   std::ifstream ifs(filepath.string());
 
   if (ifs.fail()) {
@@ -43,7 +50,8 @@ boost::optional<Location> find_section(const boost::filesystem::path &filepath, 
   std::string query("# " + target);
   std::string row;
   int row_number = 1;
-  bool is_previous_section = false; // whether previous line is section title ("# title...")
+  bool is_previous_section =
+      false; // whether previous line is section title ("# title...")
   while (std::getline(ifs, row)) {
     if (row == query) {
       return Location(filepath, row_number);
@@ -57,12 +65,15 @@ boost::optional<Location> find_section(const boost::filesystem::path &filepath, 
   return boost::none;
 }
 
-boost::optional<Location> find_glossary(const boost::filesystem::path &glospath, const std::string &target) {
+boost::optional<Location>
+find_glossary(const boost::filesystem::path &glospath,
+              const std::string &target) {
   namespace fs = boost::filesystem;
   fs::path glossary(glospath.string());
-  for (fs::recursive_directory_iterator iter = fs::recursive_directory_iterator(glossary);
-      iter != fs::end(iter);
-      iter++) {
+  for (fs::recursive_directory_iterator iter =
+           fs::recursive_directory_iterator(glossary);
+       iter != fs::end(iter);
+       iter++) {
     fs::path filepath(*iter);
     boost::optional<Location> loc(find_section(filepath, target));
     if (loc) {
@@ -72,12 +83,14 @@ boost::optional<Location> find_glossary(const boost::filesystem::path &glospath,
   return boost::none;
 }
 
-boost::optional<boost::filesystem::path> search_libpaths(const std::string &glossary, const std::vector<std::string> &libpaths) {
+boost::optional<boost::filesystem::path>
+search_libpaths(const std::string &glossary,
+                const std::vector<std::string> &libpaths) {
   namespace fs = boost::filesystem;
   for (std::string libpath : libpaths) {
     for (fs::directory_iterator iter = fs::directory_iterator(libpath);
-        iter != fs::end(iter);
-        iter++) {
+         iter != fs::end(iter);
+         iter++) {
       fs::path glospath(*iter);
       if (glospath.filename() == glossary) {
         return glospath;
@@ -87,12 +100,13 @@ boost::optional<boost::filesystem::path> search_libpaths(const std::string &glos
   return boost::none;
 }
 
-boost::optional<Location> find_library(const std::string &libpath, const std::string &target) {
+boost::optional<Location>
+find_library(const std::string &libpath, const std::string &target) {
   namespace fs = boost::filesystem;
   fs::path library(libpath);
   for (fs::directory_iterator iter = fs::directory_iterator(library);
-      iter != fs::end(iter);
-      iter++) {
+       iter != fs::end(iter);
+       iter++) {
     fs::path glospath(*iter);
     boost::optional<Location> loc(find_glossary(glospath, target));
     if (loc) {
@@ -102,7 +116,9 @@ boost::optional<Location> find_library(const std::string &libpath, const std::st
   return boost::none;
 }
 
-boost::optional<Location> find_libraries(const std::vector<std::string> &libpaths, const std::string &target) {
+boost::optional<Location>
+find_libraries(const std::vector<std::string> &libpaths,
+               const std::string &target) {
   for (std::string libpath : libpaths) {
     boost::optional<Location> location(find_library(libpath, target));
     if (location) {
@@ -112,7 +128,8 @@ boost::optional<Location> find_libraries(const std::vector<std::string> &libpath
   return boost::none;
 }
 
-std::string expand_path(const std::string &path) {
+std::string
+expand_path(const std::string &path) {
   wordexp_t p;
   int flags = 0;
   int expansion = wordexp(path.data(), &p, flags);
@@ -124,7 +141,8 @@ std::string expand_path(const std::string &path) {
   throw;
 }
 
-boost::optional<std::string> get_env(const char *key) {
+boost::optional<std::string>
+get_env(const char *key) {
   const char *value = std::getenv(key);
   if (value) {
     return std::string(value);
@@ -133,11 +151,13 @@ boost::optional<std::string> get_env(const char *key) {
   }
 }
 
-boost::optional<std::string> get_env(std::string key) {
+boost::optional<std::string>
+get_env(std::string key) {
   return get_env(key.c_str());
 }
 
-void less(const Location &l) {
+void
+less(const Location &l) {
   const boost::optional<std::string> pager(get_env("GIRLPAGER"));
   std::string cmd(pager ? *pager : "less");
   cmd += " +" + std::to_string(l.get_row_number());
@@ -145,7 +165,8 @@ void less(const Location &l) {
   system(cmd.data());
 }
 
-std::vector<std::string> getEnvs(const char *key) {
+std::vector<std::string>
+getEnvs(const char *key) {
   const boost::optional<std::string> value(get_env(key));
   std::string str(value ? *value : "");
   std::vector<std::string> vec;
@@ -153,11 +174,15 @@ std::vector<std::string> getEnvs(const char *key) {
   return vec;
 }
 
-boost::optional<Location> find_target(const boost::optional<std::string> &glossary, const std::vector<std::string> &libpaths, const std::string &target) {
+boost::optional<Location>
+find_target(const boost::optional<std::string> &glossary,
+            const std::vector<std::string> &libpaths,
+            const std::string &target) {
   if (!glossary) {
     return find_libraries(libpaths, target);
   }
-  boost::optional<boost::filesystem::path> glospath(search_libpaths(*glossary, libpaths));
+  boost::optional<boost::filesystem::path> glospath(
+      search_libpaths(*glossary, libpaths));
   if (!glospath) {
     throw;
     // return boost::none;
@@ -165,52 +190,61 @@ boost::optional<Location> find_target(const boost::optional<std::string> &glossa
   return find_glossary(*glospath, target);
 }
 
-boost::program_options::variables_map parse_commandline_arguments(int argc, char **argv) {
+boost::program_options::variables_map
+parse_commandline_arguments(int argc, char **argv) {
   namespace po = boost::program_options;
   po::options_description options("commandline options");
-  options.add_options()
-    ("glossary,g", po::value<std::string>(), "specify glossary")
-    ("query", po::value<std::vector<std::string>>(), "word to search")
-  ;
+  options.add_options()(
+      "glossary,g", po::value<std::string>(), "specify glossary")(
+      "query", po::value<std::vector<std::string>>(), "word to search");
   po::positional_options_description pod;
   pod.add("query", -1);
   po::variables_map vm;
-  po::store(
-      po::command_line_parser(argc, argv)
-        .options(options)
-        .positional(pod)
-        .run(),
-      vm);
+  po::store(po::command_line_parser(argc, argv)
+                .options(options)
+                .positional(pod)
+                .run(),
+            vm);
   po::notify(vm);
   return vm;
 }
 
-template<typename T>
-boost::optional<T> assoc(const std::string &option, const boost::program_options::variables_map &vm) {
+template <typename T>
+boost::optional<T>
+assoc(const std::string &option,
+      const boost::program_options::variables_map &vm) {
   if (!vm.count(option)) {
     return boost::none;
   }
   return vm[option].as<T>();
 }
 
-std::string concat_arguments(const std::vector<std::string> &args) {
-  return std::accumulate(args.begin(), args.end(), std::string(),
-      [](const std::string &x, const std::string y) {
-        return x.empty() ? y : x + " " + y;
-      });
+std::string
+concat_arguments(const std::vector<std::string> &args) {
+  return std::accumulate(args.begin(),
+                         args.end(),
+                         std::string(),
+                         [](const std::string &x, const std::string y) {
+                           return x.empty() ? y : x + " " + y;
+                         });
 }
 
-int main(int argc, char **argv) {
+int
+main(int argc, char **argv) {
   try {
-    boost::program_options::variables_map vm(parse_commandline_arguments(argc, argv));
+    boost::program_options::variables_map vm(
+        parse_commandline_arguments(argc, argv));
     if (!vm.count("query")) {
       std::string exec(argv[0]);
       std::cerr << "Usage: " + exec + " <word>" << std::endl;
       return 0;
     }
-    std::string target(concat_arguments(vm["query"].as<std::vector<std::string>>()));
-    boost::optional<std::string> glossary(assoc<std::string>("glossary", vm));
-    boost::optional<Location> location(find_target(glossary, getEnvs("GIRLPATH"), target));
+    std::string target(
+        concat_arguments(vm["query"].as<std::vector<std::string>>()));
+    boost::optional<std::string> glossary(
+        assoc<std::string>("glossary", vm));
+    boost::optional<Location> location(
+        find_target(glossary, getEnvs("GIRLPATH"), target));
     if (location) {
       less(*location);
     } else {
